a63cad890e9a418a829d22742cc338e3d8953db5
Andrea29-18 <aavp0316@gmail.com>
2024-12-10 18:25:21 -0600

    feat: import y export part02

---
+++ intro-javascript/src/Bases/08-imp-exp.js
@@ -0,0 +1,18 @@
+import  heroes, {owners} from '../Data/heroes';
+
+//Leer README.md para entender el ejercicio Array.find
+const getHeroById = (id) => heroes.find(hero => hero.id === id);
+
+console.log(getHeroById(2));
+
+
+//El find solo devuelve el primer elemento que cumple la condición
+const getHeroByOwner = (owner) => heroes.filter((hero) => hero.owner === owner);
+
+console.log(getHeroByOwner('DC'));
+
+//PARTE02
+
+console.log('---------------------------------');
+
+console.log(owners);
