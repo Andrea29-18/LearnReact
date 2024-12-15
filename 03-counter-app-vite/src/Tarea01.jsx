55f5ba15b48dbd5701adb42b93ca998f27f2d36f
Andrea29-18 <aavp0316@gmail.com>
2024-12-14 21:32:23 -0600

    feat; Primera tarea

---
+++ 03-counter-app-vite/src/Tarea01.jsx
@@ -0,0 +1,8 @@
+export const Tarea01 = () => {
+    return (
+        <div>
+            <h1>Hola A Todos!</h1>
+            <p>Este es un componente básico de React.</p>
+        </div>
+    );
+};
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './HelloWorldApp';
+import { Tarea01 } from './Tarea01';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
+        <Tarea01 />
     </React.StrictMode>
 );
