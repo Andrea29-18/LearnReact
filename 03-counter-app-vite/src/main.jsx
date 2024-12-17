c1eb1193de947862d04f0419496d973562738c92
Andrea29-18 <aavp0316@gmail.com>
2024-12-16 19:02:02 -0600

    feat: tarea

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from './HelloWorldApp';
 // import { Tarea01 } from './Tarea01';
-import { Props } from './Props';
+// import { Props } from './Props';
 import './styles.css';
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         {/* <App /> */}
         {/* <Tarea01 /> */}
         {/* Este es un componente padre */}
-        <Props />
+        {/* <Props /> */}
+        <CounterApp value={10} />
     </React.StrictMode>
 );
