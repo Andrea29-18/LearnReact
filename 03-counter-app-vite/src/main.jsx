1dee598a510c4a741780f5d497e9bf1b0a5b5498
Andrea29-18 <aavp0316@gmail.com>
2024-12-16 18:17:31 -0600

    feat: Styles de css

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './HelloWorldApp';
 import { Tarea01 } from './Tarea01';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
